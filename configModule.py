33051d8a13a5328bb7da7d77093b3db3b0b5d823
Marcin <marcinos180@gmail.com>
2021-05-06 00:22:31 +0200

    Saving config from is working, saving in local file in case of connection issue with server

---
+++ configModule.py
@@ -0,0 +1,42 @@
+# module responsible of saving and serving config to the rest of program
+import configparser
+import os
+from APIModule import RequestAPI
+
+
+class Config:
+
+    def __init__(self):
+        # initializing config parser and checking if config file exist
+        # if config file doesn't exist, create a new one
+        # if file exist, read config
+        self.parser = configparser.ConfigParser()
+        if not os.path.exists('config.ini'):
+            self.create_new_config()
+        else:
+            self.parser.read('config.ini')
+            self.get_config_from_server()
+
+    def create_new_config(self):
+        # when there is no config file, this function is creating a new file with empty config
+        self.parser['login'] = {'base_url': '',
+                                'license_plate': '',
+                                'password': ''}
+        self.parser['server'] = {'sendinterval': '',
+                                'getlocationinterval': ''}
+        self.parser.write(open('config.ini', 'w'))
+
+    def section_returner(self, section):
+        # returning a dictionary of requested section
+        return dict(self.parser.items(section))
+
+    def get_config_from_server(self):
+        # creating an object of API instance with login data from config.ini
+        server_API_connection = RequestAPI(self.section_returner('login'))
+        # after successful login get json and save it to config file
+        if server_API_connection.check_authorization():
+            self.parser['server'] = server_API_connection.get_config_from_server()
+            self.parser.write(open('config.ini', 'w'))
+            del(server_API_connection)
+        else:
+            pass
