d501aff9cc4e5c3e72b7ab00b4ccc7ee0361b191
Marcin <marcinos180@gmail.com>
2021-06-09 21:38:47 +0200

    BTModule class

--- BTModule.py
+++ BTModule.py
@@ -1,31 +1,39 @@
 import bluetooth
 
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-server_sock.bind(("", bluetooth.PORT_ANY))
-server_sock.listen(1)
+class Bluetooth:
 
-port = server_sock.getsockname()[1]
+    def __init__(self):
+        server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        server_sock.bind(("", bluetooth.PORT_ANY))
+        server_sock.listen(1)
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+        port = server_sock.getsockname()[1]
 
-bluetooth.advertise_service(server_sock, "CarVisor", service_id=uuid,
-                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
-                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
-                            # protocols=[bluetooth.OBEX_UUID]
-                            )
+        uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-print("Waiting for connection on RFCOMM channel", port)
+        bluetooth.advertise_service(server_sock, "CarVisor", service_id=uuid,
+                                    service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                                    profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                                    # protocols=[bluetooth.OBEX_UUID]
+                                    )
 
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from", client_info)
+    def connect(self):
+        print("Waiting for connection on RFCOMM channel", port)
 
-while True:
-    data = client_sock.recv(1024)
-    print("Received", data)
+        client_sock, client_info = server_sock.accept()
+        print("Accepted connection from", client_info)
+
+        while True:
+            data = client_sock.recv(1024)
+            print("Received", data)
+            client_sock.send("ok")
 
 
-print("Disconnected.")
+        print("Disconnected.")
 
-client_sock.close()
-server_sock.close()
-print("All done.")
+        client_sock.close()
+        server_sock.close()
+        print("All done.")
+
+bt = Bluetooth()
+bt.connect()
