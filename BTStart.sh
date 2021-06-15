b9435c0fb1e0bd5c0af5d143e611642eaf9f3d43
Marcin <marcinos180@gmail.com>
2021-06-15 21:52:50 +0200

    Updated to show BT working

--- BTStart.sh
+++ BTStart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+sudo hciconfig hci0 up
 serial=$(cat /proc/cpuinfo |grep Serial|cut -d' ' -f2)
 number=${serial: -3}
 sudo hciconfig hci0 name "CarVisor$number"
-hciconfig hci0 piscan
+sudo hciconfig hci0 piscan
