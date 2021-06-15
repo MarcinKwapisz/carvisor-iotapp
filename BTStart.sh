2b4a3c60d7021bd664ad5ad617c07448e9d54d4b
Marcin <marcinos180@gmail.com>
2021-06-15 20:07:07 +0200

    Hardware problem with uart and BT, working on solution

--- BTStart.sh
+++ BTStart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 serial=$(cat /proc/cpuinfo |grep Serial|cut -d' ' -f2)
 number=${serial: -3}
-hciconfig hci0 name "CarVisor$number"
+sudo hciconfig hci0 name "CarVisor$number"
 hciconfig hci0 piscan
