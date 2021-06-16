4fcad62e2861a02527ac13d1422b4612c035e2c5
Marcin <marcinos180@gmail.com>
2021-06-16 02:38:55 +0200

    Hot Fix

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt install git python3 python3-pip bluetooth bluez -y
+sudo apt install git python3 python3-pip bluetooth libbluetooth-dev bluez -y
 git clone https://github.com/MarcinKwapisz/carvisor-iotapp.git
 sudo pip3 install pyserial pint tinydb pybluez pynmea2
