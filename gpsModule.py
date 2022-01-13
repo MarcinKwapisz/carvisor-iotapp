9e9b175dbe03aaf20017c22b0924656b0ea0f384
Marcin <marcinos180@gmail.com>
2022-01-13 03:21:09 +0100

    Test with buzzer

--- gpsModule.py
+++ gpsModule.py
@@ -1,33 +1,41 @@
 from serial import Serial
 import pynmea2
+from gpiozero import Buzzer
+from time import sleep
 
 
 class gps:
 
     def __init__(self):
-        self.longitude = 16.5542
-        self.latitude = 52.2742
+        # self.longitude = 16.5542
+        # self.latitude = 52.2742
+        self.longitude = None
+        self.latitude = None
         self.serial = Serial("/dev/ttyAMA4", baudrate=9600, timeout=0.2)
+        self.buzzer = Buzzer(17)
 
     def gps(self):
-        self.longitude = float("%.5f" % float(self.longitude-0.0002))
-        # while True:
-        #     try:
-        #         gps_serial_line = self.serial.readline().decode("UTF-8")
-        #     except UnicodeDecodeError:
-        #         gps_serial_line = self.serial.readline()
-        #     if gps_serial_line[0:6] == '$GPRMC':
-        #         gps_output = pynmea2.parse(gps_serial_line)
-        #         if float("%.5f" % float(gps_output.longitude)) == 0.0:
-        #             pass
-        #         else:
-        #             self.latitude = float("%.5f" % float(gps_output.latitude))
-        #             self.longitude = float("%.5f" % float(gps_output.longitude))
-        #             return
-        #         if self.longitude == None:
-        #             pass
-        #         else:
-        #             return
+        # self.longitude = float("%.5f" % float(self.longitude-0.0002))
+        while True:
+            try:
+                gps_serial_line = self.serial.readline().decode("UTF-8")
+            except UnicodeDecodeError:
+                gps_serial_line = self.serial.readline()
+            if gps_serial_line[0:6] == '$GPRMC':
+                gps_output = pynmea2.parse(gps_serial_line)
+                if float("%.5f" % float(gps_output.longitude)) == 0.0:
+                    pass
+                else:
+                    self.buzzer.on()
+                    sleep(0.5)
+                    self.buzzer.off()
+                    self.latitude = float("%.5f" % float(gps_output.latitude))
+                    self.longitude = float("%.5f" % float(gps_output.longitude))
+                    return
+                if self.longitude == None:
+                    pass
+                else:
+                    return
 
     def get_only_position_values(self):
         self.gps()
