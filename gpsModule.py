c1845800edd3d3b2a7db993fa9a111a1d98ac35b
Marcin <marcinos180@gmail.com>
2022-01-13 01:46:53 +0100

    test

--- gpsModule.py
+++ gpsModule.py
@@ -5,28 +5,29 @@ import pynmea2
 class gps:
 
     def __init__(self):
-        self.longitude = None
-        self.latitude = None
+        self.longitude = 52.2742
+        self.latitude = 16.5542
         self.serial = Serial("/dev/ttyAMA4", baudrate=9600, timeout=0.2)
 
     def gps(self):
-        while True:
-            try:
-                gps_serial_line = self.serial.readline().decode("UTF-8")
-            except UnicodeDecodeError:
-                gps_serial_line = self.serial.readline()
-            if gps_serial_line[0:6] == '$GPRMC':
-                gps_output = pynmea2.parse(gps_serial_line)
-                if float("%.5f" % float(gps_output.longitude)) == 0.0:
-                    pass
-                else:
-                    self.latitude = float("%.5f" % float(gps_output.latitude))
-                    self.longitude = float("%.5f" % float(gps_output.longitude))
-                    return
-                if self.longitude == None:
-                    pass
-                else:
-                    return
+        pass
+        # while True:
+        #     try:
+        #         gps_serial_line = self.serial.readline().decode("UTF-8")
+        #     except UnicodeDecodeError:
+        #         gps_serial_line = self.serial.readline()
+        #     if gps_serial_line[0:6] == '$GPRMC':
+        #         gps_output = pynmea2.parse(gps_serial_line)
+        #         if float("%.5f" % float(gps_output.longitude)) == 0.0:
+        #             pass
+        #         else:
+        #             self.latitude = float("%.5f" % float(gps_output.latitude))
+        #             self.longitude = float("%.5f" % float(gps_output.longitude))
+        #             return
+        #         if self.longitude == None:
+        #             pass
+        #         else:
+        #             return
 
     def get_only_position_values(self):
         self.gps()
