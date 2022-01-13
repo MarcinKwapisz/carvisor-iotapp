9766ca6a711b44e2a4d4443889fddb69571d7f59
Marcin <marcinos180@gmail.com>
2022-01-13 03:01:11 +0100

    Add buzzer at waiting for NFC tag

--- nfcModule.py
+++ nfcModule.py
@@ -1,11 +1,17 @@
 import nfc
+from gpiozero import Buzzer
+from time import sleep
 
 def get_tag():
+    buzzer = Buzzer(17)
     clf = nfc.ContactlessFrontend('tty:AMA2:pn532')
     target = None
     while target is None:
+        buzzer.on()
+        sleep(0.2)
         target = clf.sense(nfc.clf.RemoteTarget('106A'), nfc.clf.RemoteTarget('106B'),
                                 nfc.clf.RemoteTarget('212F'))
+        buzzer.off()
         if target is None:
             pass
         else:
